be40f7de31946fa4903e6044847d07ca0c9be45a
pengdaqian <pengdaqian.0@bytedance.com>
2022-10-09 19:24:28 +0800

    fix

--- pretrained_weights/download.sh
+++ pretrained_weights/download.sh
@@ -1,2 +1,4 @@
 wget https://github.com/TimothyHTimothy/FAST-VQA-and-FasterVQA/releases/download/v2.0.0/FAST_VQA_B_1_4.pth
 wget https://github.com/TimothyHTimothy/FAST-VQA-and-FasterVQA/releases/download/v2.0.0/FAST_VQA_M_1_4.pth
+wget https://github.com/TimothyHTimothy/FAST-VQA-and-FasterVQA/releases/download/v2.0.1/FAST_VQA_3D_1_1_Scr.pth
+wget https://github.com/TimothyHTimothy/FAST-VQA-and-FasterVQA/releases/download/v2.0.1/FAST_VQA_B_1_4_Scr.pth
